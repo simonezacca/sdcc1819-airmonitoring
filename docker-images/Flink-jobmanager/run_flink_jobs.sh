97e748b812a9baa7b9a895ccbdb00a62e294d817
Antonio Schiazza <antonio.schiazza@gmail.com>
2020-01-09 21:40:32 +0100

    fix multiple flink job run script

--- docker-images/Flink-jobmanager/run_flink_jobs.sh
+++ docker-images/Flink-jobmanager/run_flink_jobs.sh
@@ -3,10 +3,11 @@
 sleep 30 #custom code
 if [ -n "${FLINK_CLASS}" ] && [ -n "${FLINK_JAR}" ]; then
     OLDIFS=IFS
-    IFS=',' flink_classes=$FLINK_CLASS
-    IFS=OLDIFS
+    IFS=',' 
+    flink_classes=$FLINK_CLASS
     for element in $flink_classes
         do
         exec "$FLINK_HOME"/bin/flink run -c "$element" "$FLINK_JAR" &
     done
+    IFS=$OLDIFS
 fi
