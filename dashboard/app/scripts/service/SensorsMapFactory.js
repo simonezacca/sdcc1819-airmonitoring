b7fe8dbedd5add43afb643e7571c682dced456fe
Antonio Schiazza <antonio.schiazza@gmail.com>
2020-01-10 19:36:42 +0100

    code fix

--- dashboard/app/scripts/service/SensorsMapFactory.js
+++ dashboard/app/scripts/service/SensorsMapFactory.js
@@ -24,8 +24,7 @@ mainAngularModule
                     },
                     (error) => {
                         console.error(error);
-                    },
-
+                    }
                 );
             }
 
